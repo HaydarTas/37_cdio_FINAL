43f509e2be023c6b23b2d899ff5ee3f6d7991e24
commelhi <commelhi@10.16.171.69>
2016-01-08 11:46:52 +0100

    field set ind

--- 37_cdio_FINAL/src/field/Field.java
+++ 37_cdio_FINAL/src/field/Field.java
@@ -3,3 +3,38 @@ package field;
 public class Field {
 
 }
+package entity;
+
+import entity.Player;
+
+public abstract class Field {
+	private String name;
+	private int points;
+	
+	public Field(String name){
+		this.name = name;
+	}
+
+	public Field(String name, int points) {
+		this.name = name;
+		this.points = points;
+	}
+
+	@Override
+	public String toString() {
+		return "felt [name=" + name + ", points=" + points + "]";
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public String getName() {
+		return name;
+
+	}
+
+	public abstract void landOnField(Player p);
+}
+
+
