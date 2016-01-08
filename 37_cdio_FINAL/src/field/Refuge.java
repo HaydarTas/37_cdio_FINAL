a99cf55d0e8c1d0fa2945c6b6df1a866eaac04a2
Haydar Tas <Haydar@10.16.167.17>
2016-01-08 19:36:15 +0100

    Refuge

--- 37_cdio_FINAL/src/field/Refuge.java
+++ 37_cdio_FINAL/src/field/Refuge.java
@@ -1,5 +1,29 @@
 package field;
 
-public class Refuge {
+import field.Field;
+import entity.Player;
 
+public class Refuge extends Field {
+
+	int bonus;
+	String name;
+	
+	public Refuge(String name, int bonus){
+		super(name);
+		this.bonus = bonus;
+		this.name = name;
+	}
+	public void landOnField(Player p){
+		p.addToBalance(bonus);
+		p.setInformation(2);
+	}
+
+	int getBonus() {
+		return bonus;
+	}
+
+	void setBonus(int bonus) {
+		this.bonus = bonus;
+	}
 }
+
