f70048fc510ddfa05486cedd200c74d24ea45c0e
commelhi <commelhi@10.16.171.69>
2016-01-08 10:34:46 +0100

    ok

--- 37_cdio_FINAL/src/field/GoToJail.java
+++ 37_cdio_FINAL/src/field/GoToJail.java
@@ -1,5 +1,23 @@
 package field;
 
-public class GoToJail {
+public class GoToJail extends Field {
 
+
+	private String name;
+
+	public GoToJail(String name){
+		this.name = name;
+	}
+	
+	public void landOnField(Player player){
+		
+		System.out.println("Du skal i fænsel");
+		
+		player.getPosition();
+		
+		System.out.println(player.getName() + " lander på " + player.getPosition() + "og rykkes i fængsel.");
+		
+		player.setPosition(10);
+		
+	}
 }
